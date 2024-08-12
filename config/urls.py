e74c81d27d6ca356ca53e684ef9a341b9ad35129
Riszav <riszav.01@gmail.com>
2024-08-12 20:54:06 +0600

    add statics

--- config/urls.py
+++ config/urls.py
@@ -41,6 +41,8 @@ urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
 )
 
-urlpatterns += [
+urlpatterns += ([
     path("ckeditor5/", include('django_ckeditor_5.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
